b5c4fb8af2dd638c1f635f25ad2f61c3b0955da9
Piotr Gruchała <pgruchala59@gmail.com>
2025-03-18 22:43:21 +0100

    added validation

--- lab1/techmarket-api/src/routes/productRoutes.js
+++ lab1/techmarket-api/src/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const productValidationRules = require("../middleware/validationMiddleware");
 
-router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getProductById);
-router.post("/", productController.createProduct);
-router.patch("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+router.get("/", productValidationRules.getAll, productController.getAllProducts);
+router.get("/:id", productValidationRules.getById, productController.getProductById);
+router.post("/", productValidationRules.create, productController.createProduct);
+router.patch("/:id", productValidationRules.update, productController.updateProduct);
+router.delete("/:id", productValidationRules.delete, productController.deleteProduct);
 
 module.exports = router;
